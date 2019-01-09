12c86d01c52f589a8658f9c104ac702782ccc5da
SAMSUNG <juliusz.rylko@gmail.com>
2019-01-09 17:51:06 +0100

    added user and sportObject endpoints

--- io-project-02/src/main/java/com/example/demo/rest/SportObjectRestController.java
+++ io-project-02/src/main/java/com/example/demo/rest/SportObjectRestController.java
@@ -3,6 +3,7 @@ package com.example.demo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.example.demo.entity.SportObject;
 import com.example.demo.service.SportObjectService;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api")
 public class SportObjectRestController {
 
@@ -27,9 +29,10 @@ public class SportObjectRestController {
 	}
 
 	@PostMapping("/sportObjects")
-	public void addObject(@RequestBody SportObject sportObject) {
+	public List<SportObject> addObject(@RequestBody SportObject sportObject) {
 
 		sportObjectService.addObject(sportObject);
+		return this.getSportObjects();
 
 	}
 
