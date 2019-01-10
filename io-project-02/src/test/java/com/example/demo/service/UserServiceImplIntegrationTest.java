56f5bec1baa01ffa49c9d76efc5fc13939479ad0
Sig00rd <hamletolog@gmail.com>
2019-01-10 15:44:58 +0100

    Add valid username case to UserServiceImpl IntegrationTest

--- io-project-02/src/test/java/com/example/demo/service/UserServiceImplIntegrationTest.java
+++ io-project-02/src/test/java/com/example/demo/service/UserServiceImplIntegrationTest.java
@@ -1,8 +1,51 @@
 package com.example.demo.service;
 
 import com.example.demo.SpringBootRestRegistrationApplicationTest;
+import com.example.demo.dao.UserDao;
+import com.example.demo.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserServiceImplIntegrationTest extends SpringBootRestRegistrationApplicationTest
 {
+    @TestConfiguration
+    static class UserServiceImplTestContextConfiguration {
+        @Bean
+        public UserService userService() {
+            return new UserServiceImpl();
+        }
+    }
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserDao userDao;
+
+    @Before
+    public void setUp() {
+        User seba = new User(
+                "sebaPL", "123456", "seba.galecki@gmail.com");
+
+        Mockito.when(userDao.findByUserName(seba.getUserName()))
+                .thenReturn(Optional.of(seba));
+    }
+
+    @Test
+    public void whenValidUserName_thenUserShouldBeFound() {
+        String userName = "sebaPL";
+        User found = userService.findUserByUserName(userName);
+
+        assertThat(found.getUserName())
+                .isEqualTo(userName);
+    }
 }
