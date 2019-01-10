94b4d47a4f56390632148c8b7305458e109a235c
bartlomiejzachariasz <zachariasz@student.agh.edu.pl>
2019-01-10 14:05:03 +0100

    conflict solved

--- IO-frontend/src/app/shared/game.form.ts
+++ IO-frontend/src/app/shared/game.form.ts
@@ -4,41 +4,22 @@ export class GameForm {
   cost: number;
   needed: number;
   date: string;
-<<<<<<< HEAD
   priorityDate: string;
   sportObjectName: string;
   disciplineName: string;
   pitchRoles: object;
-=======
-  sportObjectName: string;
-  disciplineName: string;
-  priorityDate: string;
-  pitchRoles: Object;
->>>>>>> master
   level: string;
 
 
   constructor(cost: number, needed: number, date: string, priorityDate: string,
-<<<<<<< HEAD
               sportObjectName: string, disciplineName: string, pitchRoles: object, level: string) {
-=======
-              sportObjectName: string, disciplineName: string, pitchRoles: Object, level: string) {
->>>>>>> master
     this.cost = cost;
     this.needed = needed;
     this.date = date;
     this.priorityDate = priorityDate;
     this.sportObjectName = sportObjectName;
-<<<<<<< HEAD
-    this.disciplineName = disciplineName;
-    this.pitchRoles = pitchRoles;
-    this.level = level;
-  }
-=======
     this.disciplineName = disciplineName.toUpperCase();
     this.pitchRoles = pitchRoles;
     this.level = level;
   }
-
->>>>>>> master
 }
