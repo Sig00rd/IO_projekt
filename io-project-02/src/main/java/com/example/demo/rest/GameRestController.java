e5fe7e5101bed968dff3665d087a4907b5ca7394
bartlomiejzachariasz <zachariasz@student.agh.edu.pl>
2019-01-13 13:18:59 +0100

    ?

--- io-project-02/src/main/java/com/example/demo/rest/GameRestController.java
+++ io-project-02/src/main/java/com/example/demo/rest/GameRestController.java
@@ -1,6 +1,21 @@
 package com.example.demo.rest;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.entity.Game;
+import com.example.demo.form.GameFilterForm;
 import com.example.demo.form.GameForm;
 import com.example.demo.service.GameService;
 import com.example.demo.wrapper.GameWrapper;
@@ -11,6 +26,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+import com.google.maps.errors.ApiException;
 
 @RestController
 @RequestMapping("/api")
@@ -65,6 +81,17 @@ public class GameRestController {
 		}
 		gameService.signUpPlayer(id, role);
 		return this.getLobby(id);
+	}
+
+	@PostMapping("/games/filter")
+	public List<GameWrapper> getFilteredGames(
+			@RequestBody GameFilterForm gameFilterForm)
+			throws ApiException, InterruptedException, IOException {
+
+		List<GameWrapper> filteredGames = gameService
+				.getFilteredGames(gameFilterForm);
+
+		return filteredGames;
 	}
 
 }
