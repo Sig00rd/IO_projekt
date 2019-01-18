ca880f7b0c5f2c4c8df114bcd543d0e75d71619a
Sig00rd <hamletolog@gmail.com>
2019-01-18 19:03:00 +0100

    Create UserPrincipleTest.java

---
+++ io-project-02/src/test/java/com/example/demo/security/UserPrincipleTest.java
@@ -0,0 +1,50 @@
+package com.example.demo.security;
+
+import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
+import com.example.demo.security.service.UserPrinciple;
+import com.example.demo.test_utils.TestUtils;
+import com.example.demo.utils.RoleName;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserPrincipleTest
+{
+    @Test
+    public void whenBuildUserPrinciple_thenResultUserPrincipleShouldRetainUserData() {
+        // given
+        User seba = TestUtils.sampleValidUser();
+
+        // when
+        UserPrinciple sebaPrinciple = UserPrinciple.build(seba);
+
+        // then
+        assertThat(sebaPrinciple.getId()).isEqualTo(seba.getId());
+        assertThat(sebaPrinciple.getUsername()).isEqualTo(seba.getUserName());
+        assertThat(sebaPrinciple.getEmail()).isEqualTo(seba.getEmail());
+        assertThat(sebaPrinciple.getPassword()).isEqualTo(seba.getPassword());
+    }
+
+    @Test
+    public void whenBuildUserPrinciple_andUsersRoleIsROLE_USER_thenGrantHimCorrectAuthorities() {
+        // given
+        User seba = TestUtils.sampleValidUser();
+        Set<Role> role_user = new HashSet<Role>();
+        role_user.add(new Role(RoleName.ROLE_USER));
+        seba.setRoles(role_user);
+
+        // when
+        UserPrinciple sebaPrinciple = UserPrinciple.build(seba);
+
+        // then
+        System.out.print(sebaPrinciple.getAuthorities());
+    }
+}
